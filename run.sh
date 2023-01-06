44db6807243bda1e039d8f9563230cfeb7cbd4ef
Joaquim Marset <joaquim.marset@gmail.com>
2023-01-06 15:08:15 +0100

    Update official metrics results

--- run.sh
+++ run.sh
@@ -1,5 +1,20 @@
 #! /bin/bash
 
-BASEDIR=D:/MAI/HLE/HLE_RBERT/experiments/experiment_8
+PREDICTIONSDIR=best_predictions
+SAVEDIR=metrics
+GROUNDTRUTH=data/raw_data/TEST_FILE_KEY.txt
 
-perl scorer.pl $BASEDIR/best_test_predictions.txt data/raw_data/TEST_FILE_KEY.txt
+
+perl scorer.pl $PREDICTIONSDIR/best_predictions_cnn.txt $GROUNDTRUTH > $SAVEDIR/metrics_cnn.txt
+
+
+perl scorer.pl $PREDICTIONSDIR/best_predictions_attention_lstm.txt $GROUNDTRUTH > $SAVEDIR/metrics_atention_lstm.txt
+
+
+perl scorer.pl $PREDICTIONSDIR/best_predictions_entity_attention.txt $GROUNDTRUTH > $SAVEDIR/metrics_entity_attention.txt
+
+
+perl scorer.pl $PREDICTIONSDIR/best_predictions_RBERT.txt $GROUNDTRUTH > $SAVEDIR/metrics_rbert.txt
+
+
+perl scorer.pl $PREDICTIONSDIR/best_predictions_attention_lstm_bert.txt $GROUNDTRUTH > $SAVEDIR/metrics_attention_lstm_bert.txt
